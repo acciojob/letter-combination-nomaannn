d97e535a70cedcbf47fc35c6c175bce41de612b7
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-03-29 18:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-function letterCombinations(input_digit) {
+
+
+var digitsToLetters = {
+  
+  2: 'abc',
+  3: 'def',
+  4: 'ghi',
+  5: 'jkl',
+  6: 'mno',
+  7: 'pqrs',
+  8: 'tuv',
+  9: 'wxyz'
+  
+};
+
+
+function letterCombinations(digits) {
   //Complete the function
+if (digits === '') {
+    return [];
+  }
+
+  var possibleValues = digits.split('').map(value => digitsToLetters[value]);
+  
+  var possibleCombinations = [];
+
+  var recursiveCombine = (start, result) => {
+
+    if (result.length === digits.length) {
+      possibleCombinations.push(result.join(''));
+    }
+
+    for (var i = start; i < possibleValues.length; i++) {
+      for (var j = 0; j < possibleValues[i].length; j++) {
+        result.push(possibleValues[i][j]);
+        recursiveCombine(i + 1, result);
+        result.pop();
+      } 
+    }
+  }
+
+  recursiveCombine(0, []);
+  return possibleCombinations;
 }
 
 module.exports = letterCombinations;
